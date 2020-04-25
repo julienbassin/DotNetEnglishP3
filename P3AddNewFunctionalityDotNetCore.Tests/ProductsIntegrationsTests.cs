dca0cff6c2861f3f11159d65eb407abcb11659e1
Julien Bassin <julienbassin@outlook.com>
2020-04-25 21:54:38 +0200

    Update Integration Tests

--- P3AddNewFunctionalityDotNetCore.Tests/ProductsIntegrationsTests.cs
+++ P3AddNewFunctionalityDotNetCore.Tests/ProductsIntegrationsTests.cs
@@ -1,20 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Moq;
-using P3AddNewFunctionalityDotNetCore.Controllers;
 using P3AddNewFunctionalityDotNetCore.Data;
 using P3AddNewFunctionalityDotNetCore.Models.Repositories;
-using P3AddNewFunctionalityDotNetCore.Models.Services;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
 using System.Linq;
 using P3AddNewFunctionalityDotNetCore.Models.Entities;
-using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
 using System.Threading.Tasks;
-using System.Net.Http;
-using System.Net;
 using System.Security.Claims;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
@@ -81,18 +73,6 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
 
     public class ProductsIntegrationsTests
     {
-        
-        //public P3Referential SetupDatabase()
-        //{
-        //    //Act
-        //    var options = new DbContextOptionsBuilder<P3Referential>()
-        //    .UseInMemoryDatabase(databaseName: "P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a")
-        //    .Options;           
-            
-        //    var context = new P3Referential(options);
-        //    return context;
-        //}
-
         [Fact]
         public void Test_Return_All_Products_In_Memory_Database()
         {
@@ -122,18 +102,7 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
                 productService.SaveProduct(product2);
                 var results = productService.GetAllProducts();
                 Assert.NotEmpty(results.ToList());
-            }           
-            //Assert
-            
-
-            //var _context = SetupDatabase();
-           
-            //var _context = new P3Referential(builder.Options);
-            //Arrange
-            //var productService = new ProductRepository(_context);
-            //var results = productService.GetAllProducts();
-            //Assert
-            //Assert.NotEmpty(results.ToList());
+            }
         }
 
         [Fact]
